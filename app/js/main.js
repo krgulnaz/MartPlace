cdd9c574add6c714059f8669edc4f0698463f01a
gulnazkabekova <krgulnoza@gmail.com>
2020-07-16 10:10:26 +0500

    start project

---
+++ app/js/main.js
@@ -0,0 +1,20 @@
+$(function() {
+
+    // $('.slider__inner').slick({
+    //     infinite: true,
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     prevArrow:'<button type="button" class=" slick-btn slick-prev"><img src="images/icons/chevron-left.svg" alt=""></button>',
+    //     nextArrow:'<button type="button" class=" slick-btn slick-next"><img src="images/icons/chevron-right.svg" alt=""></button>',
+    //     arrows:true,        
+    //   });
+
+    //   $(".rate-star").rateYo({
+    //       starWidth: "15px",
+    //       rating: 4.5,
+    //       readOnly: true
+    //     });
+
+    //     var mixer = mixitup('.product__item-inner');
+
+});
